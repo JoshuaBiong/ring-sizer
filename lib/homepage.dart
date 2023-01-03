390678fc327087a16e4cc0f77c6d51937cb5ef95
JoshBiong <joshuabiong7@gmail.com>
2023-01-04 00:57:53 +0800

    initial commit

---
+++ lib/homepage.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  double _value = 50;
+
+  void ringsizer() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Expanded(
+            flex: 2,
+            child: Container(
+              height: _value,
+              width: _value,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.blueAccent,
+              ),
+              child: Center(child: Text('${_value.round().toString()} mm')),
+            ),
+          ),
+          Expanded(
+            child: Slider(
+                max: 150,
+                min: 45,
+                value: _value,
+                onChanged: (double value) {
+                  setState(() {
+                    _value = value;
+                  });
+                }),
+          )
+        ],
+      ),
+    );
+  }
+}
